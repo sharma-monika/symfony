5e4e673c132a6730ce7f98d3527e5b5c215a54dc
msharma <monika.sharma@nerdapplabs.com>
2017-02-07 15:15:03 +0530

    Further learning about routing

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -10,28 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 class BlogController extends Controller
 {
     /**
-     * Matches /blog exactly
-     *
-     * @Route("/blog", name="blog_list")
+     * @Route("/blog/{page}", name="blog_list", requirements={"page": "\d+"})
      */
-    public function listAction()
+    public function listAction($page)
     {
        return new Response(
-            '<html><body>First Method</body></html>'
+            '<html><body>Wildcard is a digit.</body></html>'
         );
     }
 
     /**
-     * Matches /blog/*
-     *
      * @Route("/blog/{slug}", name="blog_show")
      */
     public function showAction($slug)
     {
-        // $slug will equal the dynamic part of the URL
-        // e.g. at /blog/yay-routing, then $slug='yay-routing'
         return new Response(
-            '<html><body>Second Method</body></html>'
+            '<html><body>Wildcard is not a digit.</body></html>'
         );
     }
 }
